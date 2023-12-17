14f4a12da6177f381ddffd838c5e204e33e29bd5
Juan Marqués <juanjemdc@gmail.com>
2023-12-17 18:01:44 +0100

    Day 11, part 2

--- 2023/day11-part2/src/main.rs
+++ 2023/day11-part2/src/main.rs
@@ -5,7 +5,7 @@ use std::sync::mpsc::channel;
 use std::thread;
 
 fn main() {
-    let shortest_paths = sum_shortest_paths(read_input_file());
+    let shortest_paths = sum_shortest_paths(read_input_file(), 1000000);
     println!("the sum of shortest paths is {}", shortest_paths);
 }
 
@@ -16,10 +16,10 @@ fn read_input_file() -> String {
     content
 }
 
-fn sum_shortest_paths(input: String) -> u64 {
-    let galaxies_map = parse_input(input);
-    let galaxies_map = expand_empty_rows(galaxies_map);
-    let galaxies_map = expand_empty_columns(galaxies_map);
+fn sum_shortest_paths(input: String, cost: u64) -> u64 {
+    let (galaxies_map, costs) = parse_input(input);
+    let (galaxies_map, costs) = expand_empty_rows(galaxies_map, costs, cost);
+    let (galaxies_map, costs) = expand_empty_columns(galaxies_map, costs, cost);
     let galaxies = find_galaxies_coords(galaxies_map.clone());
     let max_x = galaxies_map.len();
     let max_y = galaxies_map[0].len();
@@ -30,9 +30,10 @@ fn sum_shortest_paths(input: String) -> u64 {
         for j in i+1..galaxies.len() {
             let origin = galaxies[i];
             let destiny = galaxies[j];
+            let costs = costs.clone();
             let sender = sender.clone();
             threads.push(thread::spawn(move || {
-                sender.send(find_shortest_path(origin, destiny, max_x, max_y) as u64).unwrap();
+                sender.send(find_shortest_path(origin, destiny, max_x, max_y, costs) as u64).unwrap();
             }));
             count+=1;
         }
@@ -48,7 +49,7 @@ fn sum_shortest_paths(input: String) -> u64 {
     sum
 }
 
-fn find_shortest_path(origin: (usize, usize), destiny: (usize, usize), max_x: usize, max_y: usize) -> u32 {
+fn find_shortest_path(origin: (usize, usize), destiny: (usize, usize), max_x: usize, max_y: usize, costs: Vec<Vec<u64>>) -> u64 {
     let mut open = Vec::new();
     let mut closed = Vec::new();
     let (origin_x, origin_y) = origin;
@@ -63,13 +64,13 @@ fn find_shortest_path(origin: (usize, usize), destiny: (usize, usize), max_x: us
         if next_x == destiny_x && next_y == destiny_y {
             return cost;
         }
-        let successors = get_next_candidates(next, max_x, max_y);
+        let successors = get_next_candidates(next, max_x, max_y, costs.clone());
         let mut successors = successors.iter()
             .filter(|&&(x, y, c)| !open.iter().any(|&(nx, ny, nc)| x == nx && y==ny && nc <= c))
             .filter(|&&(x, y, c)| !open.iter().any(|&(nx, ny, nc)| x == nx && y==ny && nc <= c))
             .filter(|&&(x,y,_)| distance(x,y,destiny_x,destiny_y)<=distance(next_x,next_y,destiny_x,destiny_y))
             .copied()
-            .collect::<Vec<(usize, usize, u32)>>();
+            .collect::<Vec<(usize, usize, u64)>>();
         open.append(&mut successors);
     }
     0
@@ -79,20 +80,20 @@ fn distance(x: usize, y: usize, dx: usize, dy: usize) -> f32 {
     (((max(x,dx) - min(x,dx)).pow(2) + (max(y,dy) - min(y,dy)).pow(2)) as f32).sqrt()
 }
 
-fn get_next_candidates(previous: (usize, usize, u32), max_x: usize, max_y: usize) -> Vec<(usize, usize, u32)> {
+fn get_next_candidates(previous: (usize, usize, u64), max_x: usize, max_y: usize, costs: Vec<Vec<u64>>) -> Vec<(usize, usize, u64)> {
     let (x,y,c) = previous;
     let mut candidates = Vec::new();
     if x > 0 {
-        candidates.push((x - 1, y, c + 1));
+        candidates.push((x - 1, y, c + costs[x-1][y]));
     }
     if x+1 < max_x {
-        candidates.push((x + 1, y, c + 1));
+        candidates.push((x + 1, y, c + costs[x+1][y]));
     }
     if y > 0 {
-        candidates.push((x , y-1, c + 1));
+        candidates.push((x , y-1, c + costs[x][y-1]));
     }
     if y+1 < max_y {
-        candidates.push((x, y+1, c + 1));
+        candidates.push((x, y+1, c + costs[x][y+1]));
     }
     candidates
 }
@@ -109,13 +110,14 @@ fn find_galaxies_coords(galaxies_map: Vec<Vec<char>>) -> Vec<(usize, usize)> {
     galaxies
 }
 
-fn expand_empty_columns(galaxies_map: Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn expand_empty_columns(galaxies_map: Vec<Vec<char>>, costs: Vec<Vec<u64>>, cost: u64) -> (Vec<Vec<char>>, Vec<Vec<u64>>) {
     let galaxies_map = transpose(galaxies_map);
-    let galaxies_map = expand_empty_rows(galaxies_map);
-    transpose(galaxies_map)
+    let costs = transpose(costs);
+    let (galaxies_map, costs) = expand_empty_rows(galaxies_map, costs, cost);
+    (transpose(galaxies_map),transpose(costs))
 }
 
-fn transpose(galaxies_map: Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn transpose<T: Clone + Copy>(galaxies_map: Vec<Vec<T>>) -> Vec<Vec<T>> {
     let mut new_galaxies = Vec::new();
     for j in 0..galaxies_map[0].len() {
         let mut new_row = Vec::new();
@@ -124,22 +126,22 @@ fn transpose(galaxies_map: Vec<Vec<char>>) -> Vec<Vec<char>> {
         }
         new_galaxies.push(new_row);
     }
-    new_galaxies
+    new_galaxies.clone()
 }
 
-fn expand_empty_rows(galaxies_map: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let mut new_galaxies = Vec::new();
-    for row in galaxies_map {
+fn expand_empty_rows(galaxies_map: Vec<Vec<char>>, mut costs: Vec<Vec<u64>>, cost: u64) -> (Vec<Vec<char>>, Vec<Vec<u64>>) {
+    for (i, row) in galaxies_map.iter().enumerate() {
         if row.iter().all(|&c| c == '.') {
-            new_galaxies.push(row.clone());
+            costs[i] = vec![cost; costs[0].len()];
         }
-        new_galaxies.push(row.clone());
     }
-    new_galaxies
+    (galaxies_map,costs)
 }
 
-fn parse_input(input: String) -> Vec<Vec<char>> {
-    input.lines().map(|line| line.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>()
+fn parse_input(input: String) -> (Vec<Vec<char>>, Vec<Vec<u64>>) {
+    let vec = input.lines().map(|line| line.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
+    let costs = input.lines().map(|line| vec![1; line.len()]).collect::<Vec<Vec<u64>>>();
+    (vec,costs)
 }
 
 
@@ -160,7 +162,24 @@ mod tests {
 .......#..
 #...#....."
             .to_string();
-        let result = sum_shortest_paths(input);
-        assert_eq!(result, 374);
+        let result = sum_shortest_paths(input,10);
+        assert_eq!(result, 1030);
+    }
+
+    #[test]
+    fn test_input_2() {
+        let input = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."
+            .to_string();
+        let result = sum_shortest_paths(input,100);
+        assert_eq!(result, 8410);
     }
 }
