ebe9e212124a432898275283989fdd74c909e114
Juan Marqués <juanjemdc@gmail.com>
2024-12-11 19:19:31 +0100

    Day3, part2

---
+++ 2024/day3-part2/src/main.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const root = @import("root.zig");
+
+pub fn main() !void {
+    const input = root.readFile("input.txt") catch |err| {
+        std.log.err("{!}", .{err});
+        return err;
+    };
+    const result = try root.divideLines(input);
+    defer result.deinit();
+    var total: u32 = 0;
+    var startEnabled = true;
+    for (result.items) |line| {
+        const sum = root.sumLine(line, startEnabled);
+        total += sum.value;
+        startEnabled = sum.enabled;
+    }
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("The addition of mul instructions is: {d}\n", .{total});
+}
