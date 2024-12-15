dd5d68ba7926b71e18fe56c3ca3bcb74e6efdafc
Juan Marqués <juanjemdc@gmail.com>
2024-12-15 19:40:54 +0100

    Day 5, part 2

---
+++ 2024/day5-part2/src/main.zig
@@ -0,0 +1,62 @@
+const std = @import("std");
+const root  = @import("root.zig");
+
+pub fn main() !void {
+    const input = root.readFile("input.txt") catch |err| {std.log.err("{!}", .{err});return err;};
+    const result = try root.divideLines(input);
+    defer result.deinit();
+    var total: u32 = 0;
+    var parsingRules = true;
+    const allocator = std.heap.page_allocator;
+    var pageRules = root.rules{ .rulesMap = std.StringHashMap(std.ArrayList([]const u8)).init(allocator) };
+    defer pageRules.deinit();
+    for (result.items) |line| {
+        const cleanLine = std.mem.trim(u8, line, " \r\n");
+        if (cleanLine.len == 0) {
+            parsingRules = false;
+        } else {
+            if (parsingRules) {
+                var ruleParts = std.mem.split(u8, cleanLine, "|");
+                const firstPage = ruleParts.next().?;
+                const secondPage = ruleParts.next().?;
+                pageRules.addRule(firstPage, secondPage);
+            } else {
+                var indexMap = std.StringHashMap(usize).init(allocator);
+                defer indexMap.deinit();
+                const currentRules = pageRules.getRules();
+                var page = std.mem.splitSequence(u8, cleanLine, ",");
+                var pageElements = std.ArrayList([]const u8).init(allocator);
+                defer pageElements.deinit();
+                var pagesToVisit = std.BufSet.init(allocator);
+                defer pagesToVisit.deinit();
+                var index: usize = 0;
+                while (page.next()) |pageNumber| {
+                    try pageElements.append(pageNumber);
+                    try indexMap.put(pageNumber, index);
+                    index += 1;
+                }
+                var validPage = true;
+                var pagesIt = indexMap.keyIterator();
+                while (pagesIt.next()) |pagePointer| {
+                    const pageKey = pagePointer.*;
+                    if (currentRules.contains(pageKey)) {
+                        for (currentRules.get(pageKey).?.items) |currentPageRule| {
+                            if (indexMap.contains(currentPageRule)){
+                                validPage = validPage and indexMap.get(pageKey).? < indexMap.get(currentPageRule).?;
+                            }
+                        }
+                    }
+                }
+                if (!validPage) {
+                    var inverseIndexMap = try root.swapSort(pageElements, indexMap, currentRules);
+                    defer inverseIndexMap.deinit();
+                    const middleElement = inverseIndexMap.get(pageElements.items.len / 2).?;
+                    const elemValue = try std.fmt.parseInt(u32, middleElement, 10);
+                    total += elemValue;
+                }
+            }
+        }
+    }
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("the sum of fixed pages middle elements is: {d}\n", .{total});
+}
---
+++ 2024/day5-part2/src/root.zig
@@ -0,0 +1,162 @@
+const std = @import("std");
+const testing = std.testing;
+pub const rules = struct {
+    rulesMap: std.StringHashMap(std.ArrayList([]const u8)),
+
+    pub fn addRule(self: *rules, key: []const u8, value: []const u8) void {
+        if (!self.rulesMap.contains(key)) {
+            const allocator = std.heap.page_allocator;
+            self.rulesMap.put(key, std.ArrayList([]const u8).init(allocator)) catch unreachable;
+        }
+        var ruleList = self.rulesMap.get(key).?;
+        ruleList.append(value) catch unreachable;
+        // put the element again in the map because the pointer may be moved to another memory addres after the append;
+        self.rulesMap.put(key, ruleList) catch unreachable;
+    }
+
+    pub fn getRules(self: rules) std.StringHashMap(std.ArrayList([]const u8)) {
+        return self.rulesMap;
+    }
+
+    pub fn deinit(self: *rules) void {
+        var it = self.rulesMap.valueIterator();
+        while (it.next()) |list| {
+            list.deinit();
+        }
+        self.rulesMap.deinit();
+    }
+};
+
+pub fn divideLines(input: []const u8) !std.ArrayList([]const u8) {
+    const allocator = std.heap.page_allocator;
+    var lineList = std.ArrayList([]const u8).init(allocator);
+    var lines = std.mem.splitSequence(u8, input, "\n");
+    while (lines.next()) |line| {
+        try lineList.append(std.mem.trim(u8, line, "\r\n"));
+    }
+    return lineList;
+}
+
+pub fn readFile(path: []const u8) ![]const u8 {
+    var file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
+    return try file.readToEndAlloc(std.heap.page_allocator, @as(usize, std.math.maxInt(usize)));
+}
+
+pub fn swapSort(pageElements: std.ArrayList([]const u8), indexMap: std.StringHashMap(usize), rulesMap: std.StringHashMap(std.ArrayList([]const u8))) !std.AutoHashMap(usize, []const u8) {
+    const allocator = std.heap.page_allocator;
+    var inverseIndex = std.AutoHashMap(usize, []const u8).init(allocator);
+    var pagesIt = indexMap.keyIterator();
+    while (pagesIt.next()) |pagePointer| {
+        const pageKey = pagePointer.*;
+        try inverseIndex.put(indexMap.get(pageKey).?, pageKey);
+    }
+    var i : usize = 0;
+    while (i<pageElements.items.len-1): (i+=1) {
+        var j: usize = i+1;
+        while (j<pageElements.items.len): (j+=1) {
+            const prev = inverseIndex.get(i).?;
+            const current = inverseIndex.get(j).?;
+            if (rulesMap.contains(current)) {
+                for (rulesMap.get(current).?.items) |currentPageRule| {
+                    if (std.mem.eql(u8, prev, currentPageRule)) {
+                        try inverseIndex.put(i, current);
+                        try inverseIndex.put(j, prev);
+                    }
+                }
+            }
+        }
+    }
+    return inverseIndex;
+
+}
+
+test "test case" {
+    const input =
+        \\47|53
+        \\97|13
+        \\97|61
+        \\97|47
+        \\75|29
+        \\61|13
+        \\75|53
+        \\29|13
+        \\97|29
+        \\53|29
+        \\61|53
+        \\97|53
+        \\61|29
+        \\47|13
+        \\75|47
+        \\97|75
+        \\47|61
+        \\75|61
+        \\47|29
+        \\75|13
+        \\53|13
+        \\
+        \\75,97,47,61,53
+        \\61,13,29
+        \\97,13,75,29,47
+        \\75,47,61,53,29
+        \\97,61,53,29,13
+        \\75,29,13
+    ;
+
+    const result = try divideLines(input);
+    defer result.deinit();
+    var total: u32 = 0;
+    var parsingRules = true;
+    const allocator = std.heap.page_allocator;
+    var pageRules = rules{ .rulesMap = std.StringHashMap(std.ArrayList([]const u8)).init(allocator) };
+    defer pageRules.deinit();
+    for (result.items) |line| {
+        const cleanLine = std.mem.trim(u8, line, " \r\n");
+        if (cleanLine.len == 0) {
+            parsingRules = false;
+        } else {
+            if (parsingRules) {
+                var ruleParts = std.mem.split(u8, cleanLine, "|");
+                const firstPage = ruleParts.next().?;
+                const secondPage = ruleParts.next().?;
+                pageRules.addRule(firstPage, secondPage);
+            } else {
+                var indexMap = std.StringHashMap(usize).init(allocator);
+                defer indexMap.deinit();
+                const currentRules = pageRules.getRules();
+                var page = std.mem.splitSequence(u8, cleanLine, ",");
+                var pageElements = std.ArrayList([]const u8).init(allocator);
+                defer pageElements.deinit();
+                var pagesToVisit = std.BufSet.init(allocator);
+                defer pagesToVisit.deinit();
+                var index: usize = 0;
+                while (page.next()) |pageNumber| {
+                    try pageElements.append(pageNumber);
+                    try indexMap.put(pageNumber, index);
+                    index += 1;
+                }
+                var validPage = true;
+                var pagesIt = indexMap.keyIterator();
+                while (pagesIt.next()) |pagePointer| {
+                    const pageKey = pagePointer.*;
+                    if (currentRules.contains(pageKey)) {
+                        for (currentRules.get(pageKey).?.items) |currentPageRule| {
+                            if (indexMap.contains(currentPageRule)){
+                                validPage = validPage and indexMap.get(pageKey).? < indexMap.get(currentPageRule).?;
+                            }
+                        }
+                    }
+                }
+                if (!validPage) {
+                    var inverseIndexMap = try swapSort(pageElements, indexMap, currentRules);
+                    defer inverseIndexMap.deinit();
+                    const middleElement = inverseIndexMap.get(pageElements.items.len / 2).?;
+                    const elemValue = try std.fmt.parseInt(u32, middleElement, 10);
+                    total += elemValue;
+                }
+            }
+        }
+    }
+
+    try testing.expectEqual(123, total);
+}
