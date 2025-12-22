daa0465037ca7f47c72d96ca68c86a61fd536f63
Juan Marqués <juanjemdc@gmail.com>
2025-12-22 09:24:15 +0100

    Day 7, part 1

---
+++ 2025/day7-part1/src/lib.rs
@@ -0,0 +1,67 @@
+pub fn count_beam_splits(input: String) -> u64 {
+    let (start_point,splitter_matrix) = parse_input(input);
+    let mut visited = vec![vec![false; splitter_matrix[0].len()]; splitter_matrix.len()];
+    follow_bean(start_point,splitter_matrix, &mut visited)
+}
+fn parse_input(input: String) -> ((i32, i32), Vec<Vec<bool>>) {
+    let mut start_point = (0_i32, 0_i32);
+    for (i,line) in input.lines().enumerate() {
+        if line.contains('S') {
+            start_point = (i as i32,line.find('S').unwrap() as i32);
+            break;
+        }
+    }
+    let mut splitter_matrix = input.lines()
+        .map(|line| {line.chars().map(|c| if c == '^' {true} else {false})
+            .collect::<Vec<bool>>()})
+        .collect::<Vec<Vec<bool>>>();
+    (start_point,splitter_matrix)
+}
+
+fn follow_bean(point: (i32, i32), splitter_matrix: Vec<Vec<bool>>, visited: &mut Vec<Vec<bool>>) -> u64 {
+    if point.0 < 0 || point.1 <= 0
+        || point.0 as usize >= splitter_matrix.len()
+        || point.1 as usize >= splitter_matrix[0].len() {
+        return 0;
+    }
+    if visited[point.0 as usize][point.1 as usize] {
+        return 0;
+    }
+    visited[point.0 as usize][point.1 as usize] = true;
+    let is_splitter = splitter_matrix[point.0 as usize][point.1 as usize];
+    if is_splitter {
+        1
+            + follow_bean((point.0, point.1 - 1),splitter_matrix.clone(), visited)
+            + follow_bean((point.0, point.1 + 1),splitter_matrix, visited)
+    } else {
+        follow_bean((point.0 + 1, point.1),splitter_matrix, visited)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_input_1() {
+        let input = ".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+..............."
+            .to_string();
+        let result = count_beam_splits(input);
+        assert_eq!(result, 21);
+    }
+}
